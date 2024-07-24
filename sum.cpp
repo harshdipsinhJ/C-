fa9da49d4fa339ceb322aaa05ff3e9c8aa67f253
harshdipsinhJ <168002710+harshdipsinhJ@users.noreply.github.com>
2024-07-24 23:55:44 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "enter frist number" << endl;
+    cin >> a;
+
+    cout << "enter secound number" << endl;
+    cin >> b;
+
+    cout<<"the sum is="<< a+b<<endl;
+    cout<<"the sum is="<< a-b<<endl;
+    cout<<"the sum is="<< a*b<<endl;
+    cout<<"the sum is="<< a/b<<endl;
+    cout<<"the sum is="<< a%b<<endl;
+
+
+
+
+    return 0;
+}
